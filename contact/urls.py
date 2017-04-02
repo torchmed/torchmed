1014d867c4db2feee33e2bbde60159bb12f033fa
Raony Guimaraes <raony.guimaraes@genomicsmed.ie>
2017-04-02 02:38:37 +0100

    changes

--- contact/urls.py
+++ contact/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, url
 from contact import views
 
 urlpatterns = [
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import include, url
 
 from dashboard import views
 
